3445990fb3229d3cfc1a44045fdbfe2af5a02383
Manikgog <m4xin4t4@gmail.com>
2023-11-08 13:23:33 +0500

    Comments eliminated

--- src/main/java/com/homework/calculator/CalculatorController.java
+++ src/main/java/com/homework/calculator/CalculatorController.java
@@ -22,25 +22,59 @@ public class CalculatorController {
     @GetMapping(path = "/plus")
     public String plus(@RequestParam(value = "num1", required = false) Integer number1,
                        @RequestParam(value = "num2", required = false) Integer number2){
-        return calculatorService.plus(number1, number2);
+        String resultOfCheck = checkNumbers(number1, number2);
+        if(resultOfCheck == null){
+            Integer result = calculatorService.plus(number1, number2);
+            return number1 + " + " + number2 + " = " + result;
+        }
+        return resultOfCheck;
     }
 
     @GetMapping(path = "/minus")
     public String minus(@RequestParam(value = "num1", required = false) Integer number1,
                         @RequestParam(value = "num2", required = false) Integer number2){
-        return calculatorService.minus(number1, number2);
+        String resultOfCheck = checkNumbers(number1, number2);
+        if(resultOfCheck == null){
+            Integer result = calculatorService.minus(number1, number2);
+            return number1 + " - " + number2 + " = " + result;
+        }
+        return resultOfCheck;
     }
 
     @GetMapping(path = "/multiply")
     public String multiply(@RequestParam(value = "num1", required = false) Integer number1,
                            @RequestParam(value = "num2", required = false) Integer number2){
-        return calculatorService.multiply(number1, number2);
+        String resultOfCheck = checkNumbers(number1, number2);
+        if(resultOfCheck == null){
+            Integer result = calculatorService.multiply(number1, number2);
+            return number1 + " * " + number2 + " = " + result;
+        }
+        return resultOfCheck;
     }
 
     @GetMapping(path = "/divide")
     public String divide(@RequestParam(value = "num1", required = false) Integer number1,
                          @RequestParam(value = "num2", required = false) Integer number2){
-        return calculatorService.divide(number1, number2);
+        String resultOfCheck = checkNumbers(number1, number2);
+        if(resultOfCheck == null){
+            if(number2 == 0){
+                return "Ошибка! Делить на ноль нельзя.";
+            }
+            Integer result = calculatorService.divide(number1, number2);
+            return number1 + " / " + number2 + " = " + result;
+        }
+        return resultOfCheck;
+    }
+
+    public String checkNumbers(Integer number1, Integer number2){
+        if(number1 == null && number2 == null){
+            return "Ошибка: необходимо ввести значения.";
+        }else if(number1 == null){
+            return "Ошибка: необходимо ввести значение num1.";
+        }else if(number2 == null){
+            return "Ошибка: необходимо ввести значение num2.";
+        }
+        return null;
     }
 }
 
--- src/main/java/com/homework/calculator/CalculatorServiceImpl.java
+++ src/main/java/com/homework/calculator/CalculatorServiceImpl.java
@@ -8,52 +8,20 @@ public class CalculatorServiceImpl implements CalculatorService{
         return "Добро пожаловать в калькулятор";
     }
 
-    public String checkNumbers(Integer number1, Integer number2){
-        if(number1 == null && number2 == null){
-            return "Ошибка: необходимо ввести значения.";
-        }else if(number1 == null){
-            return "Ошибка: необходимо ввести значение num1.";
-        }else if(number2 == null){
-            return "Ошибка: необходимо ввести значение num2.";
-        }
-        return null;
-    }
-    public String plus(Integer number1, Integer number2){
-        String resultString = checkNumbers(number1, number2);
-        if(resultString != null){
-            return resultString;
-        }
-        Integer result = number1 + number2;
-        return number1 + " + " + number2 + " = " + result;
+
+    public Integer plus(Integer number1, Integer number2){
+        return number1 + number2;
     }
 
-    public String minus(Integer number1, Integer number2){
-        String resultString = checkNumbers(number1, number2);
-        if(resultString != null){
-            return resultString;
-        }
-        int result = number1 - number2;
-        return number1 + " - " + number2 + " = " + result;
+    public Integer minus(Integer number1, Integer number2){
+        return number1 - number2;
     }
 
-    public String multiply(Integer number1, Integer number2){
-        String resultString = checkNumbers(number1, number2);
-        if(resultString != null){
-            return resultString;
-        }
-        int result = number1 * number2;
-        return number1 + " * " + number2 + " = " + result;
+    public Integer multiply(Integer number1, Integer number2){
+        return number1 * number2;
     }
 
-    public String divide(Integer number1, Integer number2){
-        String resultString = checkNumbers(number1, number2);
-        if(resultString != null){
-            return resultString;
-        }
-        if(number2 == 0){
-            return "Ошибка! Делить на ноль нельзя.";
-        }
-        int result = number1 / number2;
-        return number1 + " / " + number2 + " = " + result;
+    public Integer divide(Integer number1, Integer number2){
+        return number1 / number2;
     }
 }
